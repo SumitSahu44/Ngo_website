0ad26178fdd06d5b15789aac4346d16ffa5964da
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-01-09 23:51:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Ashoka Ngo -- Sumit</title>
+	<title>Ashoka Ngo - Sumit</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="index.css?v=<?php echo time(); ?>">  <!--css file -->
 	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css?v=<?php echo time(); ?>">
