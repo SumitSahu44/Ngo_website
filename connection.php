f7c593c62d4d083db48303069fbe871f8530b351
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-01-09 23:50:19 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ngo";
+$dbname = "ngodb";
 
  $con= mysqli_connect($servername,$username,$password,$dbname);
   if(!$con)
